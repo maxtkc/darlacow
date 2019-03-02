975396da3e41128f325f15508c8feef57946c1a7
maxtkc <maxkatzchristy@gmail.com>
2019-03-02 13:14:29 -0500

    no more bluetooth

--- scripts/start_audio.sh
+++ scripts/start_audio.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # MUST NOT BE ROOT
-pactl load-module module-bluetooth-discover
-pactl load-module module-switch-on-connect
 
-bluetoothctl << EOF
-connect C0:28:8D:93:06:3B
-EOF
+# BLUETOOTH
+#pactl load-module module-bluetooth-discover
+#pactl load-module module-switch-on-connect
+#
+#bluetoothctl << EOF
+#connect C0:28:8D:93:06:3B
+#EOF
 
 mpd &
